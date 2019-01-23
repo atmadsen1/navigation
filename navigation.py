f7111f17e2d21455fc73c81dd90af4807ba9500e
Kyle Melton <kylemelton32@gmail.com>
2019-01-23 02:22:12 -0700

    Adding in cameracapture object

--- navigation.py
+++ navigation.py
@@ -4,7 +4,7 @@ class Frame:
     """
     Starts the calculations done to locate the balls in the frame.
     :param frame: numpy array that is provided from cv2:VideoCapture:read()
-    :param color_range: a dictionary in the form of 
+    :param color_range: a dictionary in the form of
         {color_identifier : (hsv_min, hsv_max) } as calculated from
         range-detector.py
     :param frame_count: optional interger used to describe the frame number
@@ -25,7 +25,7 @@ class Frame:
 
     """
     Get the center of the ball that is identified by the color(arg1).
-    Note: This uses cv2.minEnclosingCircle(). This result can be 
+    Note: This uses cv2.minEnclosingCircle(). This result can be
     different by several pixels from the moment center. The difference
     between these two methods is documented at https://docs.opencv.org/3.1.0/d3/dc0/group__imgproc__shape.html#ga8ce13c24081bbc7151e9326f412190f1.
     """
@@ -48,4 +48,28 @@ class Frame:
         if self.frameProcessor.get_balls() is not None:
             return self.frameProcessor.get_balls().get(color)[3]
         return -1
+
+class CameraCapture:
+    def __init__(self):
+        pass
+
+    def get_current_position(self):
+        pass
+
+class Navigation(threading.Thread):
+
+    def __init__(self, autonomy):
+        self.autonomy = autonomy
+        self.run_flag = True
+        self.camera = CameraCapture()
+
+    def run(self):
+        while self.run_flag:
+            position = camera.get_current_position()
+            autonomy.update_current_position(position)
+
+
+if __name__ == "__main__":
+    pass
+
 
